0c027db724097270a75fbf0e760e7b0e60179ed0
Ubuntu <ubuntu@ip-172-31-26-52.us-west-1.compute.internal>
2015-11-17 11:45:06 +0000

    Scrape scripts

---
+++ download_images.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import urllib
+from time import sleep
+import re
+import concurrent.futures
+from multiprocessing import Pool
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+def get_painting_jpg(url_pair,jpeg_dir='image_dir/'):
+    the_link,the_filename = url_pair.split(",")
+    if the_link and the_filename:
+        urllib.urlretrieve(the_link,jpeg_dir+the_filename+'.jpg')
+        sleep(1)
+        return the_link
+    else:
+        return "No page"
+
+def kick_off(style):
+    links_filename = style + '_links.txt'
+    print "Running download for: ", style
+    with open(links_filename) as f:
+        painting_links_names = f.read().splitlines()
+    for a_chunk in chunks(painting_links_names,len(painting_links_names)/10):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            future_to_url = {executor.submit(get_painting_jpg,url): url for url in a_chunk}
+            for future in concurrent.futures.as_completed(future_to_url):
+                url = future_to_url[future]
+                try:
+                    data = future.result()
+                except Exception as exc:
+                    #print('MY_ERROR: %r generated an exception: %s' % (url, exc))
+                    pass
+                else:
+                    #print('MY_INFO: %r page is %d bytes' % (url, len(data)))
+                    pass
+    print "Done with:", style
+    return a_chunk[0]
+
+if __name__ == '__main__':
+
+    #kick_off('impressionism')
+    styles_to_scrape_parallel = [ "photorealism", "contemporary-realism", \
+                                 "american-realism", "hyper-realism", "post-impressionism", \
+                                 "pointillism", "cloisonnism", "fauvism", "intimism", "cubism", "cubo-futurism", \
+                                 "cubo-expressionism", "tubism", "transavantgarde", "transautomatism", "mechanistic-cubism", \
+                                 "futurism", "abstract-art", "abstract-expressionism","realism","impressionism"]
+    my_pool = Pool(len(styles_to_scrape_parallel))
+    run_results = my_pool.map( kick_off, styles_to_scrape_parallel) 
+    print "COMPLETE" 
--- scrape_for_images.py
+++ scrape_for_images.py
@@ -6,6 +6,8 @@ import concurrent.futures
 from time import sleep
 import re
 from multiprocessing import Pool
+import sys
+from collections import defaultdict
 
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:42.0) Gecko/20100101 Firefox/42.0",\
     "Referer":    "http://www.wikiart.org/en/paintings-by-style",\
@@ -23,6 +25,7 @@ def to_file_name(its_link):
     return "__".join(its_link.split("/")[-2:])
 
 def to_soup(a_link):
+    #print "In soup"
     s = requests.Session()
     s.headers.update(headers)
     response = s.get(a_link)
@@ -38,22 +41,24 @@ class ImageScrape(object):
     def build_df(self):
         for a_page in self._collect_pages():
             for a_painting_link in self._collect_links_to_paintings(a_page):
-                #print a_painting_link
                 a_painting_df = self._scrape_painting_link(a_painting_link)
-                self.paintings_df = self.paintings_df.append(a_painting_df)
+                self.paintings_df = self.paintings_df.append(a_painting_df,ignore_index=True)
+            if len(self.paintings_df) >= 4000:
+                print self.gallery_type, "Limit reached"
+                break
         self.paintings_df['from'] = self.gallery_type
 
     def save_locally(self,df_file,links_file):
         #Save pandas dataframe locally
-        self.paintings_df.to_pickle(df_file)
-        #Write a file with jpeg_url,jpeg_name
+        df = self.paintings_df
+        df.to_pickle(df_file)
         with open(links_file, 'w') as f:
-            for a_jpeg_url,a_jpeg_name in zip(list(self.paintings_df.jpeg_url),list(self.paintings_df.local_jpeg)):
-                f.write(a_jpeg_url + ',' + a_jpeg_name + '\n')
+            for a_jpeg_url,a_jpeg_name in zip(list(self.paintings_df['jpeg_url']),list(self.paintings_df['local_jpeg'])):
+                f.write(a_jpeg_url.encode(errors='ignore') + ',' + a_jpeg_name.encode(errors='ignore') + '\n')
 
     def _collect_pages(self):
         soup = to_soup(self.gallery_page)
-        print self.gallery_page
+        #print self.gallery_page
         page_num_list = [int(x.text) for x in soup.select('.pager-items a') if x.text != 'Next' and x.text != 'Previous' and x.text != '...']
         if page_num_list:
             max_page_num = max(page_num_list)
@@ -67,29 +72,39 @@ class ImageScrape(object):
 
     def _scrape_painting_link(self,a_painting_link):
         soup = to_soup(a_painting_link)
-        painting_dict = {}
-        painting_dict['link_to'] = [a_painting_link]
-        painting_dict['jpeg_url'] = [soup.select('#paintingImage')[0]['href']]
-        painting_dict['local_jpeg'] = [to_file_name(a_painting_link)]
-        painting_dict['Title'] = [soup.find('h1').findChildren()[0].getText()]
-        for a_row in soup.select('.DataProfileBox p'):
-            k,v = "".join(a_row.getText().splitlines()).split(":")
-            if k in image_df_cols:
-                painting_dict[k] = [v]
+        print a_painting_link
+        painting_dict = dict(zip(image_df_cols,[""]*len(image_df_cols)))
+        try:
+            painting_dict['link_to'] = [a_painting_link]
+            painting_dict['jpeg_url'] = [soup.select('#paintingImage')[0]['href']]
+            painting_dict['local_jpeg'] = [to_file_name(a_painting_link)]
+            painting_dict['Title'] = [soup.find('h1').findChildren()[0].getText()]
+            for a_row in soup.select('.DataProfileBox p'):    
+                if re.search(':', a_row.getText()):
+                    k,v = "".join(a_row.getText().splitlines()).split(":")
+                    k,v = k.strip(),v.strip()
+                    if k in image_df_cols:
+                        painting_dict[k] = [v]
+        except:
+            #print "Raised exception, ",a_painting_link
+            pass
+        else:
+            #print "Raised error, ",a_painting_link
+            pass
         return pd.DataFrame(data=painting_dict,columns=image_df_cols)
 
 def call_image_scrape(scrape_style):
-    a_scraper = ImageScrape(a_style)
+    a_scraper = ImageScrape(scrape_style)
     a_scraper.build_df()
-    a_scraper.build_df()
-    a_scraper.save_locally(a_style+'.pkl', a_style+'_links.txt')
+    a_scraper.save_locally(scrape_style+'.pkl', scrape_style+'_links.txt')
+    return (scrape_style+'.pkl',scrape_style+'_links.txt')
     
-
 if __name__ == '__main__':
 
-    styles_to_scrape = \
-    ["photorealism", "contemporary-realism", "american-realism", "hyper-realism", "realism", "impressionism", "post-impressionism", "pointillism", "cloisonnism", "fauvism", "intimism", "cubism", "cubo-futurism", "cubo-expressionism", "tubism", "transavantgarde", "transautomatism", "mechanistic-cubism", "futurism", "abstract-art", "abstract-expressionism"]
+    #print call_image_scrape('pointillism')
+    styles_to_scrape_parallel = \
+    ["photorealism", "contemporary-realism", "american-realism", "hyper-realism", "post-impressionism", "pointillism", "cloisonnism", "fauvism", "intimism", "cubism", "cubo-futurism", "cubo-expressionism", "tubism", "transavantgarde", "transautomatism", "mechanistic-cubism", "futurism", "abstract-art", "abstract-expressionism","realism","impressionism"]
+    
+    my_pool = Pool(len(styles_to_scrape_parallel))
+    run_results = my_pool.map( call_image_scrape, styles_to_scrape_parallel) 
 
-    my_pool = Pool(40)
-
-    run_results = my_pool.map( call_image_scrape, styles_to_scrape) 
